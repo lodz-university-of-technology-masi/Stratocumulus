4ca4086d4b61304789c8190897c0165d2797bb76
Etahel <44235258+Etahel@users.noreply.github.com>
2019-11-12 18:51:51 +0100

    Added grid to MainView, basic button functions

---
+++ View/MainView.js
@@ -0,0 +1,15 @@
+function showTests() {
+    var x = document.getElementById("UserListId");
+    var y = document.getElementById("TestListId");
+
+    x.style.display = "none";
+    y.style.display = "block";
+}
+
+function showUsers() {
+    var x = document.getElementById("TestListId");
+    var y = document.getElementById("UserListId");
+
+    x.style.display = "none";
+    y.style.display = "block";
+}
