f122e3b63b5a118675b82ef67e49ac6289c56d93
Shumitu <darkdragon5255@gmail.com>
2019-11-12 23:49:20 +0100

    add: view and edit candidate view

---
+++ View/view-and-edit-candidate.js
@@ -0,0 +1,31 @@
+
+
+function showUserData() {
+
+    var userName = document.getElementById("userName");
+    var userSurname = document.getElementById("userSurname");
+    var userEmail = document.getElementById("userEmail");
+    var userBirthDate = document.getElementById("userBirth");
+    var userPhoneNum = document.getElementById("userPhoneNum");
+
+    //toDo: load data from database....
+
+    userName.value = "Jan";
+    userSurname.value = "Kowalski";
+    userEmail.value = "jan.kowalski@gmail.com";
+    userBirthDate.value = "2000-11-11";
+    userPhoneNum.value = "123123123";
+}
+
+function saveUserData() {
+
+    var userName = document.getElementById("userName").value;
+    var userSurname = document.getElementById("userSurname").value;
+    var userEmail = document.getElementById("userEmail").value;
+    var userBirthDate = document.getElementById("userBirth").value;
+    var userPhoneNum = document.getElementById("userPhoneNum").value;
+
+    //toDo save data to database...
+
+
+}
