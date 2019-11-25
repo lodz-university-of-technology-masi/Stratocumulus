631028484bc2d31101e3bd1cf3deb3b055e8b332
Etahel <44235258+Etahel@users.noreply.github.com>
2019-11-25 03:26:24 +0100

    jqery, demonstration of importing views into main template

--- View/MainView.js
+++ View/MainView.js
@@ -13,3 +13,39 @@ function showUsers() {
     x.style.display = "none";
     y.style.display = "block";
 }
+
+function addElement(name, link) {
+    var y = document.getElementById("UserListId");
+
+    if(y.style.display=="none") {
+
+        var hyper = document.createElement("a");
+        var li = document.createElement("li");
+        var textnode = document.createTextNode("name");
+
+        hyper.setAttribute("href", link)
+        hyper.appendChild(textnode);
+        li.appendChild(hyper)
+        document.getElementById("TestListId").appendChild(li);
+    }
+    else {
+        var hyper = document.createElement("a");
+        var li = document.createElement("li");
+        var textnode = document.createTextNode("name");
+
+        hyper.setAttribute("href", link)
+        hyper.appendChild(textnode);
+        li.appendChild(hyper)
+        document.getElementById("UserListId").appendChild(li);
+    }
+
+}
+
+$(document).ready(function(){
+    $("button").click(function(){
+        $("#includedContent").load("new-test.html");
+    });
+});
+
+
+
