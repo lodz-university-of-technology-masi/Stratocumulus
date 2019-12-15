c5de88ef377319947fdd2d957aa64b3036bef31f
Shumitu <darkdragon5255@gmail.com>
2019-12-15 20:43:18 +0100

    add: missing catalogs

--- View/js/poolConfig.js
+++ View/js/poolConfig.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_bVFwGt3kK', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6275bau024uo3ngfftl7vib1a0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_CY4O3GKHV', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'thcc01b1nkqm7fti3p434r7un', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
