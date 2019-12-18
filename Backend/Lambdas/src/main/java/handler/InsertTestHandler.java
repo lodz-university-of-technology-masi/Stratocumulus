cbd4dc981a557bce43291855886511ce066a9305
Adrian Warcholinski <adrianwarcholinski9@gmail.com>
2019-12-18 13:46:16 +0100

    Adapt InsertTest lambda to Proxy Integration

--- Backend/Lambdas/src/main/java/handler/InsertTestHandler.java
+++ Backend/Lambdas/src/main/java/handler/InsertTestHandler.java
@@ -2,32 +2,47 @@ package handler;
 
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Item;
-import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
-import com.amazonaws.services.dynamodbv2.document.spec.PutItemSpec;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import dynamodb.DynamoDBUtils;
-import request.InsertTestRequest;
+import model.Test;
+import org.json.JSONObject;
+import request.RequestInput;
+import request.RequestOutput;
 
-public class InsertTestHandler implements RequestHandler<InsertTestRequest, String> {
+public class InsertTestHandler implements RequestHandler<RequestInput, RequestOutput> {
 
     private static final String TABLE_NAME = "Tests";
     private DynamoDB dynamoDB = DynamoDBUtils.getDynamoDB();
 
     @Override
-    public String handleRequest(InsertTestRequest test, Context context) {
-        insertTest(test);
+    public RequestOutput handleRequest(RequestInput input, Context context) {
+        Test test = new Test(input.getBody());
 
-        return "Inserted successfully";
+        RequestOutput output = new RequestOutput();
+        output.setStatusCode(200);
+
+        JSONObject responseJson = new JSONObject();
+        responseJson.put("id", test.getId());
+
+        insertToDatabase(test);
+
+        output.setBody(test.toString());
+        output.setStatusCode(200);
+
+//        JSONObject responseJson = new JSONObject(input.getBody());
+//        RequestOutput output = new RequestOutput();
+//        output.setBody(responseJson.toString());
+//        output.setStatusCode(200);
+
+        return output;
     }
 
-    private PutItemOutcome insertTest(InsertTestRequest test) {
-        return dynamoDB.getTable(TABLE_NAME).putItem(new PutItemSpec().withItem(
-                new Item()
-                        .withString("id", test.getId())
-                        .withString("name", test.getName())
-                        .withString("language", test.getLanguage())
-                        .withJSON("questions", test.getQuestions())
-        ));
+    private void insertToDatabase(Test test) {
+        dynamoDB.getTable(TABLE_NAME).putItem(new Item()
+                .withString("id", test.getId())
+                .withString("name", test.getName())
+                .withString("language", test.getLanguage())
+                .withJSON("questions", test.getQuestionsJson()));
     }
 }
