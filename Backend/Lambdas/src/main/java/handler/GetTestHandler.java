5bd42ee2ab9d187dba24b3c0f612427f8e5bcbee
Adrian Warcholinski <adrianwarcholinski9@gmail.com>
2019-12-28 12:24:23 +0100

    Add GET endpoint for tests

---
+++ Backend/Lambdas/src/main/java/handler/GetTestHandler.java
@@ -0,0 +1,27 @@
+package handler;
+
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import dynamodb.DynamoDBUtils;
+import request.RequestInput;
+import request.RequestOutput;
+
+public class GetTestHandler implements RequestHandler<RequestInput, RequestOutput> {
+
+    private Table table = DynamoDBUtils.getDynamoDB().getTable("Tests");
+
+    @Override
+    public RequestOutput handleRequest(RequestInput requestInput, Context context) {
+        String id = requestInput.getQueryStringParameters().get("id");
+
+        Item foundItem = table.getItem("id", id);
+
+        RequestOutput output = new RequestOutput();
+        output.setBody(foundItem.toJSON());
+        output.setStatusCode(200);
+
+        return output;
+    }
+}
