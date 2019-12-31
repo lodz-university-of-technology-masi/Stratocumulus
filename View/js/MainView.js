d59a844c0df7bfa9708c297871172e489fd8b4f6
Etahel <44235258+Etahel@users.noreply.github.com>
2019-12-31 14:44:33 +0100

    Filling test list on load

--- View/js/MainView.js
+++ View/js/MainView.js
@@ -26,7 +26,79 @@ function showAddView() {
     }
 }
 
+function onPageLoad() {
+    var poolData = {
+        UserPoolId: 'us-east-1_CY4O3GKHV',
+        ClientId: 'thcc01b1nkqm7fti3p434r7un'
+    };
 
+    var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+
+    var email = 'adrianwarcholinski9@gmail.com';
+    var password = 'Qwerty123';
+
+    var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails({
+        Username: email,
+        Password: password
+    });
+
+    var cognitoUser = new AmazonCognitoIdentity.CognitoUser({
+        Username: email,
+        Pool: userPool
+    });
+
+    cognitoUser.authenticateUser(authenticationDetails, {
+        onSuccess: function printOkMessage() {
+            console.log('Authenticated successfully')
+        },
+        onFailure: function printFailedMessage() {
+            console.log('Authentication failed')
+        }
+    });
+
+    var idToken;
+
+    cognitoUser.getSession(function (err, session) {
+        if (err) {
+            console.log('Error');
+        } else {
+            console.log(':)')
+            idToken = session.getIdToken().getJwtToken();
+        }
+    });
+
+    let response = '';
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+
+        if (this.readyState === 4 && this.status === 200) {
+            response = this.responseText;
+            console.log(this.responseText);
+            populateTestList(JSON.parse(response));
+        }
+    };
+
+    xhttp.open("GET", "https://ot28vqg79h.execute-api.us-east-1.amazonaws.com/dev/tests", true);
+    xhttp.setRequestHeader('Content-Type', 'application/json');
+    xhttp.setRequestHeader('Authorization', idToken);
+    xhttp.send();
+
+
+}
+
+function populateTestList(testList)
+{
+
+    for (let i = 0; i < testList.length; i++) {
+        let btn = document.createElement("BUTTON");
+        btn.innerHTML = testList[i].name;
+
+        let li = document.createElement("li");
+        li.appendChild(btn);
+        document.getElementById("TestListId").appendChild(li);
+    }
+}
 
 
 
