9f2cd4d4ffffd6eb161b86a92aefc832184c84de
Shumitu <darkdragon5255@gmail.com>
2020-01-02 17:32:08 +0100

    add: GET with users in pool

--- View/js/MainView.js
+++ View/js/MainView.js
@@ -18,48 +18,10 @@ function showUsers() {
 
 function onPageLoad() {
     reloadList();
+    getUserList();
 }
 
 function reloadList() {
-    var poolData = {
-        UserPoolId: 'us-east-1_CY4O3GKHV',
-        ClientId: 'thcc01b1nkqm7fti3p434r7un'
-    };
-
-    var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
-
-    var email = 'adrianwarcholinski9@gmail.com';
-    var password = 'Qwerty123';
-
-    var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails({
-        Username: email,
-        Password: password
-    });
-
-    var cognitoUser = new AmazonCognitoIdentity.CognitoUser({
-        Username: email,
-        Pool: userPool
-    });
-
-    cognitoUser.authenticateUser(authenticationDetails, {
-        onSuccess: function printOkMessage() {
-            console.log('Authenticated successfully')
-        },
-        onFailure: function printFailedMessage() {
-            console.log('Authentication failed')
-        }
-    });
-
-    var idToken;
-
-    cognitoUser.getSession(function (err, session) {
-        if (err) {
-            console.log('Error');
-        } else {
-            console.log(':)')
-            idToken = session.getIdToken().getJwtToken();
-        }
-    });
 
     let response = '';
 
@@ -75,7 +37,7 @@ function reloadList() {
 
     xhttp.open("GET", "https://ot28vqg79h.execute-api.us-east-1.amazonaws.com/dev/tests", true);
     xhttp.setRequestHeader('Content-Type', 'application/json');
-    xhttp.setRequestHeader('Authorization', idToken);
+    xhttp.setRequestHeader('Authorization', getAccessToken());
     xhttp.send();
 
 
@@ -124,3 +86,67 @@ document.getElementById( "AlertMsg").parentElement.style.display="inline-block";
 
 }
 
+function getAccessToken() {
+
+    var poolData = {
+        UserPoolId: 'us-east-1_CY4O3GKHV',
+        ClientId: 'thcc01b1nkqm7fti3p434r7un'
+    };
+
+    var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+
+    var email = 'adrianwarcholinski9@gmail.com';
+    var password = 'Qwerty123';
+
+    var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails({
+        Username: email,
+        Password: password
+    });
+
+    var cognitoUser = new AmazonCognitoIdentity.CognitoUser({
+        Username: email,
+        Pool: userPool
+    });
+
+    cognitoUser.authenticateUser(authenticationDetails, {
+        onSuccess: function printOkMessage() {
+            console.log('Authenticated successfully')
+        },
+        onFailure: function printFailedMessage() {
+            console.log('Authentication failed')
+        }
+    });
+
+    var idToken;
+
+    cognitoUser.getSession(function (err, session) {
+        if (err) {
+            console.log('Error');
+        } else {
+            console.log(':)')
+            idToken = session.getIdToken().getJwtToken();
+        }
+    });
+
+    return idToken;
+}
+
+function getUserList() {
+    let userList = '';
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+
+
+        if (this.readyState === 4 && this.status === 200) {
+            userList = this.responseText;
+            console.log(this.responseText);
+        }
+    };
+
+    xhttp.open("GET", "https://ot28vqg79h.execute-api.us-east-1.amazonaws.com/dev/candidates", true);
+
+    xhttp.setRequestHeader('Content-Type', 'application/json');
+    xhttp.setRequestHeader('Authorization', getAccessToken());
+
+    xhttp.send();
+}
